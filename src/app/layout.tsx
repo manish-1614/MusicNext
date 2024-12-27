73398af49a4500816da46b24b9d01128e96fdf3c
Manish Kumar Prajapati <myindiaman@gmail.com>
2024-12-27 18:52:10 +0530

    Initial commit to prepare a template repository

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Roboto, Roboto_Mono } from "next/font/google";
+import "./globals.css";
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+});
+
+const robotoFont = Roboto({
+  variable: "--font-roboto",
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Next App Base model",
+  description: "This is the base model template for Next.js projects",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${robotoFont.variable} ${robotoMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
